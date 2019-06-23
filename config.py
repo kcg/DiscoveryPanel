291bd140ef21c4abef6eb6086908b458fe022b65
Karl C. Goedel <mail@karl-goedel.de>
2019-06-23 21:41:31 +0200

    Added a comment to config regarding aliases.

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 processlist = [
     {"name": "Counting", "command": "./testscript.sh", "autostart": False},
     {"name": "Stress", "command": "stress --cpu 1", "autostart": False},
-    {"name": "Gedit", "command": "gedit", "autostart": False},
-	{"name": "Calendar", "command": "cal", "autostart": False},
-	{"name": "Testit", "command": "testit", "autostart": False}
+	{"name": "Testit", "command": "/bin/bash -i -c testit", "autostart": False} # The /bin/bash -i -c is necessary to be able to load aliases from e.g. bashrc
+    {"name": "Gedit", "command": "gedit", "autostart": False}
 ]
