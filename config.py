d6c327b8dd386a18a258b15759a1052b52831e0c
Karl C. Goedel <mail@karl-goedel.de>
2019-06-23 22:05:01 +0200

    Added screenshot and fixed missing comma in config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 processlist = [
     {"name": "Counting", "command": "./testscript.sh", "autostart": False},
     {"name": "Stress", "command": "stress --cpu 1", "autostart": False},
-	{"name": "Testit", "command": "/bin/bash -i -c testit", "autostart": False} # The /bin/bash -i -c is necessary to be able to load aliases from e.g. bashrc
+	{"name": "Testit", "command": "/bin/bash -i -c testit", "autostart": False}, # The /bin/bash -i -c is necessary to be able to load aliases from e.g. bashrc
     {"name": "Gedit", "command": "gedit", "autostart": False}
 ]
